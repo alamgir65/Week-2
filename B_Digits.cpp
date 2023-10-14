15c60dd51eb6e4795c6682b27744959d80dc09a0
alamgir65 <122733174+alamgir65@users.noreply.github.com>
2023-10-14 19:37:11 +0600

    Add files via upload

---
+++ B_Digits.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int n,k; cin>>n>>k;
+    int cnt=0;
+    while(n!=0){
+        n=n/k;
+        cnt++;
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
---
+++ C_Sum_of_gcd_of_Tuples_Easy.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int n; cin>>n;
+    long long sum=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<=n;k++){
+                sum += __gcd(i,__gcd(j,k));
+            }
+        }
+    }
+    cout<<sum<<endl;
+    return 0;
+}
